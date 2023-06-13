a46ef89bd4300a1128278e47dfe45dfa720cee7c
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-06-13 17:07:13 -0300

    aula 03

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,7 +13,11 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item;
+  editando = false;
+  textoBtn = 'Salvar item';
+
   valorItem!: string;
   constructor(private listaService: ListaDeCompraService) {}
 
@@ -19,5 +30,23 @@ export class InputComponent implements OnInit {
 
   limparCampo() {
     this.valorItem = '';
+  }
+
+  editarItem() {
+    this.listaService.editarItemDaLista(
+      this.itemQueVaiSerEditado,
+      this.valorItem
+    );
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['itemQueVaiSerEditado'].firstChange) {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
   }
 }
